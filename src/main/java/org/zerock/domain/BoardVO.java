6ef3cec8070e8b1a2b40a5c9b32db478ac9a72a9
cuteprettykara <cuteprettykara@gmail.com>
2019-04-24 10:32:03 +0900

    Part3 chapter 8. 기본적인 웹 게시물 관리 - Persistence 계층의 CRUD 구현

---
+++ src/main/java/org/zerock/domain/BoardVO.java
@@ -0,0 +1,62 @@
+package org.zerock.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+
+	private long bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private Date updateDate;
+	
+	public BoardVO(Long bno, String title, String content, String writer) {
+		this.bno = bno;
+		this.title = title;
+		this.content = content;
+		this.writer = writer;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BoardVO other = (BoardVO) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (writer == null) {
+			if (other.writer != null)
+				return false;
+		} else if (!writer.equals(other.writer))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((writer == null) ? 0 : writer.hashCode());
+		return result;
+	}
+	
+	
+}
---
+++ src/test/java/org/zerock/mapper/BoardMapperTest.java
@@ -0,0 +1,86 @@
+package org.zerock.mapper;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.domain.BoardVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTest {
+	
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	
+	public static final long TEST_BNO = 1L;
+
+	@Test
+	public void getList() {
+		assertThat(1, is(mapper.getList().size()));
+		
+		mapper.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void insert() {
+		BoardVO board = new BoardVO(0L, "새로 작성하는 글", "새로 작성하는 내용", "newbie");
+		
+		assertThat(mapper.insert(board), is(1));
+		
+		log.info("board: " + board);
+	}
+	
+	@Test
+	public void insertSelectKey() {
+		BoardVO board = new BoardVO(0L, "새로 작성하는 글", "새로 작성하는 내용", "newbie");
+		
+		assertThat(mapper.insertSelectKey(board), is(1));
+		
+		log.info("board: " + board);
+	}
+	
+	@Test
+	public void read() {
+		BoardVO board = new BoardVO(0L, "테스트 제목", "테스트 내용", "prettykara");
+		
+		BoardVO actual = mapper.read(TEST_BNO);
+				
+		assertEquals(board, actual);
+		
+		log.info("actual: " + actual);
+	}
+	
+	@Test
+	public void delete() throws Exception {
+		assertThat(mapper.delete(TEST_BNO), is(1));
+		
+		assertNull(mapper.read(TEST_BNO));
+	}
+	
+	
+	  @Test 
+	  public void update() throws Exception { 
+		  BoardVO board = new BoardVO(TEST_BNO, "update 제목", "update 내용", "update writer");
+		  assertThat(mapper.update(board), is(1));
+		  
+		  BoardVO actual = mapper.read(TEST_BNO); 
+		  assertEquals(board, actual); 
+		  
+		  log.info("board: " + board);
+		  log.info("actual: " + actual);
+	  }
+	 
+}
