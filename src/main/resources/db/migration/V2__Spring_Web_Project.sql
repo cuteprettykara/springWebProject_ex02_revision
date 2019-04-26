07771d3d7f4cea8ab6abb1bc90110abfda3e8cbf
cuteprettykara <cuteprettykara@gmail.com>
2019-04-26 17:44:14 +0900

    flyway db migration 2차 (tbl_board 샘플 데이터 추가)

---
+++ src/main/resources/db/migration/V2__Spring_Web_Project.sql
@@ -0,0 +1,53 @@
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
+
+insert into tbl_board (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board);
