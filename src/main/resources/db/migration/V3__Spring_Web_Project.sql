7292aa426ac7d3b1b96e422f0e0f39e41be5e707
cuteprettykara <cuteprettykara@gmail.com>
2019-05-06 16:46:02 +0900

    flyway db migration 3차 (tbl_reply 생성)

---
+++ src/main/resources/db/migration/V3__Spring_Web_Project.sql
@@ -0,0 +1,18 @@
+create table tbl_reply ( 
+	rno number(10, 0),
+	bno number(10, 0) NOT NULL,
+	replytext varchar2(1000) NOT NULL, 
+	replyer varchar2(50) NOT NULL, 
+	regDate date default sysdate,
+	updateDate date default sysdate
+);
+
+create sequence seq_reply;
+
+alter table tbl_reply add constraint pk_reply primary key (rno);
+
+alter table tbl_reply add constraint fk_reply_board
+foreign key (bno) refereneces tbl_board(bno);
+
+insert into tbl_reply (rno, bno, replytext, replyer)
+values(seq_reply.nextval, 1, '댓글 테스트1', 'replyer1');
