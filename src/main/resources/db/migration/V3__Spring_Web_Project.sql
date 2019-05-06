0d9438158d52bdc66e9542df3a238cf9331b4669
cuteprettykara <cuteprettykara@gmail.com>
2019-05-06 16:58:28 +0900

    오타 수정

--- src/main/resources/db/migration/V3__Spring_Web_Project.sql
+++ src/main/resources/db/migration/V3__Spring_Web_Project.sql
@@ -12,7 +12,7 @@ create sequence seq_reply;
 alter table tbl_reply add constraint pk_reply primary key (rno);
 
 alter table tbl_reply add constraint fk_reply_board
-foreign key (bno) refereneces tbl_board(bno);
+foreign key (bno) references tbl_board(bno);
 
 insert into tbl_reply (rno, bno, replytext, replyer)
 values(seq_reply.nextval, 1, '댓글 테스트1', 'replyer1');
