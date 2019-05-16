5e4939be95d8043918d907ccc699c8fb7289bcdb
cuteprettykara <cuteprettykara@gmail.com>
2019-05-16 12:01:25 +0900

    버그 수정

--- src/main/java/org/zerock/service/BoardServiceImpl.java
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -61,7 +61,7 @@ public class BoardServiceImpl implements BoardService {
 		
 		boolean modifyResult = mapper.update(board) == 1;
 		
-		if (modifyResult && board.getAttachList().size() > 0) {
+		if (modifyResult && board.getAttachList() != null && board.getAttachList().size() > 0) {
 			board.getAttachList().forEach(attach -> {
 				attach.setBno(board.getBno());
 				attachMapper.insert(attach);
